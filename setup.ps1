65b5c8d0748612989cfd768a9ab1412bff7acd22
tntdog99 <epicghast12@gmail.com>
2026-09-13 18:41:41 -0600

    added pwsh, powershell to setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -22,3 +22,13 @@ if ((Get-Command wt.exe -ErrorAction SilentlyContinue)) {
     wt -d client2
     wt -d client3
 }
+if ((Get-Command pwsh.exe -ErrorAction SilentlyContinue)) {
+    Start-Process pwsh -WorkingDirectory $PWD/client1
+    Start-Process pwsh -WorkingDirectory $PWD/client2
+    Start-Process pwsh -WorkingDirectory $PWD/client3
+}
+if ((Get-Command powershell.exe -ErrorAction SilentlyContinue)) {
+    Start-Process powershell -WorkingDirectory $PWD/client1
+    Start-Process powershell -WorkingDirectory $PWD/client2
+    Start-Process powershell -WorkingDirectory $PWD/client3
+}
