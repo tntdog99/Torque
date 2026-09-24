6ca90ed499d98188c7b48adb23b7cf5311de4a22
tntdog99 <epicghast12@gmail.com>
2026-09-24 14:58:25 -0600

    updated buildcli.*

--- buildcli.sh
+++ buildcli.sh
@@ -6,5 +6,5 @@ source .venv/bin/activate
 pip install -r cli_client/requirements.txt
 pip install pyinstaller
 pyinstaller --onefile --paths="." --paths="deps" --distpath "dist/linux/cli_client/" --copy-metadata readchar ./cli_client/main.py 
-cp -R shared/* dist/cli_client
-cp -R shared/.storage dist/cli_client/
+cp -R shared/* dist/linux/cli_client
+cp -R shared/.storage dist/linux/cli_client/
