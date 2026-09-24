a3d4390943be3cdf68cb56d643972b6b0b0bf4cd
tntdog99 <epicghast12@gmail.com>
2026-09-24 14:49:11 -0600

    updated buildcli.*

--- buildcli.bat
+++ buildcli.bat
@@ -5,4 +5,4 @@ call .venv\Scripts\activate.bat
 pip install -r cli_client\requirements.txt
 pip install pyinstaller
 pyinstaller --onefile --paths="." --paths="deps" --distpath "dist/windows/cli_client/" --copy-metadata readchar .\cli_client\main.py 
-xcopy shared dist\cli_client /e /h /r /y
+xcopy shared dist\windows\cli_client /e /h /r /y
